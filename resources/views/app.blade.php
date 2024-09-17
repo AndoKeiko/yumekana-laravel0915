2befc9b1faa60503c9e32a6d7aa1759fc526bbe4
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-17 21:52:54 +0900

    app.bladeからvite削除2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My SPA Application</title>
     <!-- 必要なスタイルやスクリプトを読み込む -->
-    <!-- @vite('resources/css/app.css') -->
 </head>
 <body>
     <div id="app"></div>
     <!-- ビルドされた JavaScript ファイルを読み込む -->
-    <!-- @viteReactRefresh
-    @vite('resources/js/app.jsx') -->
 </body>
 </html>
