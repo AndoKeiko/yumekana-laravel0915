aaf70973551db3faa5c2f083f1ce7cf1bbfdd789
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-20 10:50:36 +0900

    開発環境に戻す苦労

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
   'paths' => ['api/*', 'sanctum/csrf-cookie'],
   'allowed_methods' => ['*'],
-  'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5174,https://gajumaro.sakura.ne.jp')),
+  'allowed_origins' => ['http://localhost:5174', 'https://gajumaro.sakura.ne.jp'],
   'allowed_headers' => ['*'],
   'exposed_headers' => [],
   'max_age' => 0,
