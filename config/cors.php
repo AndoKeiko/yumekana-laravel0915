95b105b3fb099c3055707c191560055f2d5bff0f
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-24 11:54:31 +0900

    401-5

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
   'paths' => ['api/*', 'sanctum/csrf-cookie'],
   'allowed_methods' => ['*'],
-  'allowed_origins' => ['*'],
+  'allowed_origins' => ['https://gajumaro.sakura.ne.jp', 'http://localhost:5174'],
   'allowed_headers' => ['*'],
   'supports_credentials' => true,
 ];
