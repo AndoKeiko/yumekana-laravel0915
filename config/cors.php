3b9e7ba921df13f5a5e556dbcf93a74d406ccad8
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-24 14:46:02 +0900

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-  'paths' => ['api/*', 'sanctum/csrf-cookie'],
-  'allowed_methods' => ['*'],
-  'allowed_origins' => ['https://gajumaro.sakura.ne.jp'],
-  'allowed_headers' => ['*'],
-  'supports_credentials' => true,
+  'paths' => ['api/*', 'sanctum/csrf-cookie'], // APIエンドポイントとCSRFクッキーのためのパス
+  'allowed_methods' => ['*'], // 必要なHTTPメソッドを指定
+  'allowed_origins' => ['https://gajumaro.sakura.ne.jp'], // フロントエンドのURLを指定
+  'allowed_origins_patterns' => [],
+  'allowed_headers' => ['*'], // 必要に応じて指定
+  'exposed_headers' => [],
+  'max_age' => 0,
+  'supports_credentials' => true, // 認証情報（Cookieなど）を送信するために必須
 ];
