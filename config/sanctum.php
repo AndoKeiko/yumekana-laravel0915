3c22191397c6c80e4aa433c7fb817593cb11bba6
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-24 16:03:48 +0900

    apiの認証戻してみた9

--- config/sanctum.php
+++ config/sanctum.php
@@ -5,9 +5,11 @@ use Laravel\Sanctum\Sanctum;
 return [
 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'gajumaro.sakura.ne.jp')),
   'prefix' => '',
+  'guard' => ['web'],
   'expiration' => null,
   'middleware' => [
       'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
       'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
   ],
+  'domain' => env('SESSION_DOMAIN', 'gajumaro.sakura.ne.jp'),
 ];
