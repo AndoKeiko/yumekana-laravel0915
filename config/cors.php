31022b0bf3453b6850d42e9517846c3ca03048a1
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-24 15:39:00 +0900

    apiの認証戻してみた3

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,9 @@
 <?php
 
 return [
-  'paths' => ['api/*', 'sanctum/csrf-cookie'], // APIエンドポイントとCSRFクッキーのためのパス
-  'allowed_methods' => ['*'], // 必要なHTTPメソッドを指定
-  'allowed_origins' => ['https://gajumaro.sakura.ne.jp'], // フロントエンドのURLを指定
-  'allowed_origins_patterns' => [],
-  'allowed_headers' => ['*'], // 必要に応じて指定
-  'exposed_headers' => [],
-  'max_age' => 0,
-  'supports_credentials' => true, // 認証情報（Cookieなど）を送信するために必須
+  'paths' => ['api/*', 'sanctum/csrf-cookie'],
+  'allowed_methods' => ['*'],
+  'allowed_origins' => ['https://gajumaro.sakura.ne.jp'],  // フロントエンドのURLを許可
+  'allowed_headers' => ['*'],
+  'supports_credentials' => true,  // クッキーを共有するために必要
 ];
