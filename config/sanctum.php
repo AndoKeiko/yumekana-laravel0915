bef39784b93c6689ab994c3b8d1678b8fafced15
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-09-24 09:39:11 +0900

    401対策3

--- config/sanctum.php
+++ config/sanctum.php
@@ -3,9 +3,10 @@
 use Laravel\Sanctum\Sanctum;
 
 return [
-'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5174,gajumaro.sakura.ne.jp')),
-  'guard' => ['web'],
-'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 60 * 24),
+  'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5174,gajumaro.sakura.ne.jp')),
+  'guard' => ['web', 'api'],
+  // 'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 60 * 24),
+  'expiration' => null,
   'middleware' => [
     'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
     'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
